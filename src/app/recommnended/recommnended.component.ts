5a985290967b2d032089920520c376ba3e781a02
Gary Bermudez <garybm.17@gmail.com>
2019-04-05 10:21:52 -0700

    complete the model Product, and add a ngFor to display all instances of the product model in the HTML doc :) yay.So happy

--- src/app/recommnended/recommnended.component.ts
+++ src/app/recommnended/recommnended.component.ts
@@ -8,7 +8,7 @@ import { Product } from '../models/product.model';
 })
 export class RecommnendedComponent implements OnInit {
 
-  productOne: Product[] = [
+  products: Product[] = [
     new Product('Bose Home Speaker 500',399.95,'Best in class wall-to-wall stereo sound. Amazon Alexa built in.','Bluetooth'),
     new Product('BQC20 noise cancelling headphones – Apple devices',224.95,'Noise cancelling, inline mic/remote, Aware mode','Noise cancelling'),
     new Product('A20® Aviation Headset',995.95,'Around-ear, noise reduction, clear audio, audio/communication interface','Noise cancelling'),
@@ -17,7 +17,7 @@ export class RecommnendedComponent implements OnInit {
 
   constructor() { }
 
+
   ngOnInit() {
   }
-
 }
